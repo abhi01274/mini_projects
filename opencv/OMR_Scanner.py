42f5eab8ace252a980a192e1fbd2b7fdcbb9d22b
abhishek <abhishekgarg01274@gmail.com>
2019-07-22 23:37:08 +0530

    OMR_updated

--- opencv/OMR_Scanner.py
+++ opencv/OMR_Scanner.py
@@ -2,15 +2,10 @@
 from imutils.perspective import four_point_transform
 from imutils import contours
 import numpy as np
-import argparse
 import imutils
 import cv2
  
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
-args = vars(ap.parse_args())
+
  
 # define the answer key which maps the question number
 # to the correct answer
@@ -18,7 +13,7 @@ ANSWER_KEY = {0: 1, 1: 4, 2: 0, 3: 3, 4: 1}
 
 # load the image, convert it to grayscale, blur it
 # slightly, then find edges
-image = cv2.imread(args["image"])
+image = cv2.imread("#path")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(blurred, 75, 200)
